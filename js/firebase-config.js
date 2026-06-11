1fc433c0dd9f976798f5a88313edbf07ce7c8474
Karthick Raja P <karthickraja23122004@gmail.com>
2026-06-11 14:40:41 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,10 +3,11 @@
 const firebaseConfig = {
   apiKey: "AIzaSyCvUhJcB20lwo3qvOFM0lAopwRVNOMWVX4",
   authDomain: "my-sentinel-5e573.firebaseapp.com",
+  databaseURL: "https://my-sentinel-5e573-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "my-sentinel-5e573",
   storageBucket: "my-sentinel-5e573.firebasestorage.app",
   messagingSenderId: "734370576144",
-  appId: "1:734370576144:web:a42aed0c6e977dcf891795"
+  appId: "YOUR_APP_ID"
 };
 // Check if Firebase credentials have been configured by the user
 function isFirebaseConfigured() {
